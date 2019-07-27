8e7b8da2b487e036cd044b8acaec4b21eb2d5527
ridharid <53091196+ridharid@users.noreply.github.com>
2019-07-27 22:09:37 +0700

    Add files via upload

---
+++ Soal3.php
@@ -0,0 +1,19 @@
+<?php>
+
+function cetak_gambar($ukuran) {
+        $char1 = "*";
+        $char2 = "=";
+        $ukuran = 9;
+        $onehalf = floor($ukuran/2);
+        $onehalf_var = $onehalf
+        
+        //header
+        console.log(" ".repeat($ukuran / 2) + "--- panjang ---" + " ".repeat($ukuran / 2));
+
+        for($baris=0; $baris<=$ukuran) {
+            if($ukuran % 2 == 0) {
+                echo "*"
+            }
+        }
+}
+?>
